0889c75ae683e856c1e63171c31eec386586e21f
Caracal.js <59297610+caracal-js@users.noreply.github.com>
2022-02-25 21:17:27 -0500

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -6,5 +6,5 @@ self.__uv$config = {
     handler: '/ultraviolet/uv.handler.js',
     bundle: '/ultraviolet/uv.bundle.js',
     config: '/ultraviolet/uv.config.js',
-    sw: '/ultraviolet/uv.sw.js',
+    sw: '/ultraviolet/uv.sw-handler.js',
 };
